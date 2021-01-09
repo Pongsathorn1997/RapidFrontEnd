95c48ad97953da9b4a31aea80fd6a7f29cc5451c
Pongsathorn1997 <59756685+Pongsathorn1997@users.noreply.github.com>
2021-01-09 17:46:41 +0700

    update this thing again

---
+++ script.js
@@ -0,0 +1,25 @@
+// function myFunction() {
+        //     var x = document.getElementById("email").innerHTML;
+        //     document.getElementById("demo").innerHTML = "Hello World";
+        // }
+
+function insertEmail(){
+    var email = document.getElementById('email').value
+    console.log(email);
+    console.log(typeof(email));
+    return checkEmail(email);
+}
+
+function validEmail(email){
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+function checkEmail(email){
+    if(validEmail(email)){
+        document.getElementById('c2').style.display = "none"
+        return document.getElementById('myModal').style.display = "block"
+    }else
+        return document.getElementById('demo').innerHTML = "A valid email is required";
+}
+
